f3c33cc75395ca16b44789244281b9ccbc42d380
martinagalletti <martinagalletti@icloud.com>
2020-09-18 14:50:25 +0200

    one more file

---
+++ preprocess.py
@@ -0,0 +1,25 @@
+def get_data_from_file(train_file, batch_size, seq_size):
+    with open(train_file, 'r') as f:
+        text = f.read()
+    text = text.split()
+
+    word_counts = Counter(text)
+    sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+    int_to_vocab = {k: w for k, w in enumerate(sorted_vocab)}
+    vocab_to_int = {w: k for k, w in int_to_vocab.items()}
+    n_vocab = len(int_to_vocab)
+
+    print('Vocabulary size', n_vocab)
+
+    int_text = [vocab_to_int[w] for w in text]
+    num_batches = int(len(int_text) / (seq_size * batch_size))
+    in_text = int_text[:num_batches * batch_size * seq_size]
+    out_text = np.zeros_like(in_text)
+    out_text[:-1] = in_text[1:]
+    out_text[-1] = in_text[0]
+    in_text = np.reshape(in_text, (batch_size, -1))
+    out_text = np.reshape(out_text, (batch_size, -1))
+    return int_to_vocab, vocab_to_int, n_vocab, in_text, out_text
+
+    print(in_text[:10, :10])
+    print(out_text[:10, :10])
